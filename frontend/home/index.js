3e820bdd3b5d99ec6527083202569bb80382a2b5
flowingfate <shang542361224@163.com>
2016-11-11 22:40:54 +0800

    hahah

--- frontend/home/index.js
+++ frontend/home/index.js
@@ -9,19 +9,22 @@ import Laboratory from './src/homeCross-lab/index';
 import Activity from './src/homeCross-activity/index';
 import Footer from './src/footer/index';
 
+import $ from "jquery";
+
 if(module.hot) 
 {
 	module.hot.accept();
 }
 
-
 function timeout(ms) {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms, 'promise done');
+    setTimeout(()=>{
+    	resolve('this is a promise test');
+    }, ms);
   });
 }
 
-timeout(100).then((value) => {
+timeout(5000).then((value) => {
   console.log(value);
 });
 
